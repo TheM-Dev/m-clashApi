fc57be0d0ac631e5b467666e6e2fc00432e0c0fd
Michał Kornela <themdev@protonmail.com>
2022-11-17 08:53:59 +0100

    Added new function to API. getUser gathers all data about user.

--- README.md
+++ README.md
@@ -15,3 +15,35 @@ Simple scraper for easier access to the Clash Royale (Supercell) API data.
         ...
     ]
     ```
+- getUser() - Get back an object with ALL possible to gather user data.
+    Example response: 
+    ```js
+    {
+        profile: {
+            name: name,
+            tag: tag,
+            level: expLevel,
+            trophies: trophies,
+            wins: wins,
+            losses: losses,
+            battles: battleCount,
+            threeCrownWins: threeCrownWins,
+            totalDonations: totalDonations,
+            clan: {
+                name: clan name,
+                tag: clan tag
+            },
+            arena: arenaname,
+            badges: badges,
+            achievements: achievements,
+            cards: cards,
+            currentDeck: currentDeck,
+            currentFavouriteCard: currentFavouriteCard,
+            starPoints: starPoints,
+            expPoints: expPoints,
+            totalExpPoints:  totalExpPoints
+        },
+        chestCycle: chestData,
+        battleLog: battleData
+    };
+    ```
--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const axios = require('axios');
 module.exports = function(options){
     if(!options || !options.token) return new Error(`You need to provide API token in options!`)
     this.apiToken = options.token;
-    this.requestOptions = { headers: { 'Authorization': this.apiToken } }
+    this.request = async (link) => {
+        const requestOptions = { headers: { 'Authorization': this.apiToken } };
+        return await axios.get(link, this.requestOptions);
+    }
     /**
      * @name                getAllCards
      * @description         Get back an array with all cards.
@@ -12,13 +15,50 @@ module.exports = function(options){
      * @returns             { Array }
      */
     getAllCards: async () => {
-        const res = await axios.get(`https://api.clashroyale.com/v1/cards`, this.requestOptions);
-        const response = [];
-        const cards = res.data.items;
+        const data = await this.request(`https://api.clashroyale.com/v1/cards`);
+        const response = []; const cards = data.data.items;
         cards.forEach((c) => {
             const { name, id, maxLevel, iconUrls } = c;
             response.push({ id, name, maxLevel, icon: iconUrls.medium });
         });
+        return response;
+    }
+    /**
+     * @name                getUser
+     * @description         Get back an object with ALL possible to gather user data.
+     * @category            Profile
+     * @author              m.
+     * @returns             { Object }
+     */
+    getUser: async (playerTag) => {
+        const profileData = await this.request(`https://api.clashroyale.com/v1/players/%23${playerTag}`).data;
+        const chestData = await this.request(`https://api.clashroyale.com/v1/players/%23${playerTag}/upcomingchests`).data;
+        const battleData = await this.request(`https://api.clashroyale.com/v1/players/%23${playerTag}/battlelog`).data;
+        const response = {
+            profile: {
+                name: profileData.name,
+                tag: profileData.tag,
+                level: profileData.expLevel,
+                trophies: profileData.trophies,
+                wins: profileData.wins,
+                losses: profileData.losses,
+                battles: profileData.battleCount,
+                threeCrownWins: profileData.threeCrownWins,
+                totalDonations: profileData.totalDonations,
+                clan: { name: profileData.clan.name, tag: profileData.clan.tag },
+                arena: profileData.arena.name,
+                badges: profileData.badges,
+                achievements: profileData.achievements,
+                cards: profileData.cards,
+                currentDeck: profileData.currentDeck,
+                currentFavouriteCard: profileData.currentFavouriteCard,
+                starPoints: profileData.starPoints,
+                expPoints: profileData.expPoints,
+                totalExpPoints: profileData. totalExpPoints
+            },
+            chestCycle: chestData.items,
+            battleLog: battleData
+        };
         return response;
     }
 }
