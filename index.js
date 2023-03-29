4564eff584010edb7e7dbd52bbdbd121daecdaca
jakub-wasik <jakub.wasik@schibsted.com>
2023-03-29 23:10:18 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-// You don't need axios TBH. Just use `fetch` API
-const axios = require('axios');
+// You could write it as a class or just functions and then combine it into the exported object tbh (syntactic sugar).
+// Writing modules this way is very oldschool
+module.exports = ({ token }) => { // if the token is the only value then it can be just (token) instead of options object. But it is okay that way as well
+    if (!token) return new Error(`You need to provide API token in options!`);
 
-// Could just destructure the token
-// You could write it as a class tbh (syntactic sugar). Writing modules this way is very oldschool
-module.exports = function (options) {
-    if (!options || !options.token) return new Error(`You need to provide API token in options!`) // missing ; use eslint/prettier
-
-    // no need to asigning the API token here to `this`. Or it could be OK if that's something you want to provide as your API to access a token.
-    // But if the user provides the token to the instance of this module, then he can probably get it and manage in a better way than you do.
-    this.apiToken = options.token;
-
-    // Use fetch API
-    // `get` is more meaningfull than `request`, i'd change the `link` to `url` as well
-    // you don't need async here, you can just return a promise anyways and await it where you consume it
-    this.request = async (link) => {
-        const requestOptions = { headers: { 'Authorization': this.apiToken } };
-        // just use `requestOptions`
-        return await axios.get(link, this.requestOptions);
-    }
+    const BASE_URL = `https://api.clashroyale.com/v1`;
+    // you can do generic error handling here
+    const get = (url) => fetch(`${BASE_URL}${url}`, { headers: { 'Authorization': token } })
+        .then(res => res.json());
 
     // Nice to see some JSdocs ;) Tho they do not describe exactly what's returned :P
     /**
@@ -27,27 +16,14 @@ module.exports = function (options) {
      * @author              m.
      * @returns             { Array }
      */
-    getAllCards: async () => {
-        // no proper error handling.
-        const data = await this.request(`https://api.clashroyale.com/v1/cards`);
 
-        // No need to create empty array for response, just use Array.map()
-        // because of axios u need to access it via `data.data.items` instead of `data.items`
-        // also u could have destructured it
-        // i see no need to keep two consts in one line, either use const once and declare two variables or just move it to the next line
-        // i'd move it to the next line because it is more readable. Don't try to make your code shorter by sacrificing the readability.
-        const response = []; const cards = data.data.items;
+    // no proper error handling.
+    // Also you may just try to spread { ...c, icon: c.iconUrls.medium } if those are the only properties available in card
+    // But tbh explicitly declaring which properties you want to use is safer (no unnecessary data is spilled - ex. api might return more data in the future)
+    // Just return the promise, no need for async/await
+    const getAllCards = () => get(`/cards`)
+        .then(cards => cards.map(({ id, name, maxLevel, iconUrls }) => ({ id, name, maxLevel, icon: iconUrls.medium })));
 
-        // Use Array.map()
-        // return cards.map(({ id, name, maxLevel, iconUrls }) => ({ id, name, maxLebel, icon: iconUrls.medium }));
-        // Also you may just try to spread { ...c, icon: c.iconUrls.medium } if those are the only properties available in card
-        // But tbh explicitly declaring which properties you want to use is safer (no unnecessary data is spilled - ex. api might return more data in the future)
-        cards.forEach((c) => {
-            const { name, id, maxLevel, iconUrls } = c;
-            response.push({ id, name, maxLevel, icon: iconUrls.medium });
-        });
-        return response;
-    }
 
     // Same here, not quite known what type are the returned fields in the object (what is the shape of the object returned)
     // And also what is the expected type of `playerTag` param
@@ -58,39 +34,59 @@ module.exports = function (options) {
      * @author              m.
      * @returns             { Object }
      */
-    getUser: async (playerTag) => {
+    const getUser = async (playerTag) => {
         // no error handling
-        const profileData = await this.request(`https://api.clashroyale.com/v1/players/%23${playerTag}`).data;
-        const chestData = await this.request(`https://api.clashroyale.com/v1/players/%23${playerTag}/upcomingchests`).data;
-        const battleData = await this.request(`https://api.clashroyale.com/v1/players/%23${playerTag}/battlelog`).data;
-        // just return { ... }, no need to declare `response` variable here.
-        // Also you could destructure some properties to save a lot of code and improve readability.
-        // remember you can rename variables when destructuring
-        const response = {
-            profile: {
-                name: profileData.name,
-                tag: profileData.tag,
-                level: profileData.expLevel,
-                trophies: profileData.trophies,
-                wins: profileData.wins,
-                losses: profileData.losses,
-                battles: profileData.battleCount,
-                threeCrownWins: profileData.threeCrownWins,
-                totalDonations: profileData.totalDonations,
-                clan: { name: profileData.clan.name, tag: profileData.clan.tag },
-                arena: profileData.arena.name,
-                badges: profileData.badges,
-                achievements: profileData.achievements,
-                cards: profileData.cards,
-                currentDeck: profileData.currentDeck,
-                currentFavouriteCard: profileData.currentFavouriteCard,
-                starPoints: profileData.starPoints,
-                expPoints: profileData.expPoints,
-                totalExpPoints: profileData.totalExpPoints
-            },
+        const chestData = await get(`/players/%23${playerTag}/upcomingchests`);
+        const battleLog = await get(`/players/%23${playerTag}/battlelog`);
+        // It could be spread but this way is safer to not spill as mentioned above
+        const {
+            name,
+            tag,
+            expLevel: level,
+            trophies,
+            wins,
+            losses,
+            battleCount: battles,
+            threeCrownWins,
+            totalDonations,
+            clan,
+            arena,
+            badges,
+            achievements,
+            cards,
+            currentDeck,
+            currentFavouriteCard,
+            starPoints,
+            expPoints,
+            totalExpPoints
+        } = await get(`/players/%23${playerTag}`);
+
+        return {
             chestCycle: chestData.items,
-            battleLog: battleData
+            battleLog,
+            profile: {
+                name,
+                tag,
+                level,
+                trophies,
+                wins,
+                losses,
+                battles,
+                threeCrownWins,
+                totalDonations,
+                clan: { name: clan.name, tag: clan.tag },
+                arena: arena.name,
+                badges,
+                achievements,
+                cards,
+                currentDeck,
+                currentFavouriteCard,
+                starPoints,
+                expPoints,
+                totalExpPoints,
+            },
         };
-        return response;
-    }
-}
+    };
+
+    return { getAllCards, getUser };
+};
